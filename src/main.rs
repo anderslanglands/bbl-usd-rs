4a39e00f502d413c515ab837002e0c2f544f55ec
Anders Langlands <anderslanglands@gmail.com>
2023-12-06 22:01:32 +1300

    print out more attr types

--- src/main.rs
+++ src/main.rs
@@ -1,4 +1,4 @@
-use bbl_usd::usd::{self, Object, PropertyKind};
+use bbl_usd::usd::{self, Attribute, Object, PropertyKind};
 use glam::{Vec2, Vec3, Vec4};
 
 pub fn main() {
@@ -12,196 +12,200 @@ pub fn main() {
         for prop in prim.properties().iter() {
             match prop.property_kind() {
                 PropertyKind::Attribute(attr) => {
-                    if let Some(value) = attr.get() {
-                        if let Some(token) = value.as_token() {
-                            println!(
-                                "  {}: {} = {}",
-                                attr.name().text(),
-                                attr.type_name().as_token(),
-                                token
-                            );
-                        } else if let Some(val) = value.as_asset_path() {
-                            let path = if val.resolved_path().is_empty() {
-                                val.asset_path()
-                            } else {
-                                val.resolved_path()
-                            };
-
-                            println!(
-                                "  {}: {} = @{}@",
-                                attr.name().text(),
-                                attr.type_name().as_token(),
-                                path
-                            );
-                        } else if let Some(val) = value.get::<i32>() {
-                            println!(
-                                "  {}: {} = {}",
-                                attr.name().text(),
-                                attr.type_name().as_token(),
-                                val
-                            );
-                        } else if let Some(val) = value.get::<f32>() {
-                            println!(
-                                "  {}: {} = {}",
-                                attr.name().text(),
-                                attr.type_name().as_token(),
-                                val
-                            );
-                        } else if let Some(val) = value.get::<f64>() {
-                            println!(
-                                "  {}: {} = {}",
-                                attr.name().text(),
-                                attr.type_name().as_token(),
-                                val
-                            );
-                        } else if let Some(val) = value.get::<bool>() {
-                            println!(
-                                "  {}: {} = {}",
-                                attr.name().text(),
-                                attr.type_name().as_token(),
-                                val
-                            );
-                        } else if let Some(val) = value.get::<Vec2>() {
-                            println!(
-                                "  {}: {} = {}",
-                                attr.name().text(),
-                                attr.type_name().as_token(),
-                                val
-                            );
-                        } else if let Some(val) = value.get::<Vec3>() {
-                            println!(
-                                "  {}: {} = {}",
-                                attr.name().text(),
-                                attr.type_name().as_token(),
-                                val
-                            );
-                        } else if let Some(val) = value.get::<Vec4>() {
-                            println!(
-                                "  {}: {} = {}",
-                                attr.name().text(),
-                                attr.type_name().as_token(),
-                                val
-                            );
-                        } else if let Some(val) = value.as_token_array() {
-                            println!(
-                                "  {}: {} = [{}]",
-                                attr.name().text(),
-                                attr.type_name().as_token(),
-                                val.iter()
-                                    .map(|x| x.text())
-                                    .collect::<Vec<&str>>()
-                                    .join(", ")
-                            );
-                        } else if let Some(val) = value.as_int_array() {
-                            let size = val.size();
-                            let elide = if size < 8 { "" } else { "..." };
-                            println!(
-                                "  {}: {} = {}:[{}{}]",
-                                attr.name().text(),
-                                attr.type_name().as_token(),
-                                size,
-                                val.iter()
-                                    .take(size.min(8))
-                                    .map(|x| format!("{}", x))
-                                    .collect::<Vec<String>>()
-                                    .join(", "),
-                                elide
-                            );
-                        } else if let Some(val) = value.as_float_array() {
-                            let size = val.size();
-                            let elide = if size < 8 { "" } else { "..." };
-                            println!(
-                                "  {}: {} = {}:[{}{}]",
-                                attr.name().text(),
-                                attr.type_name().as_token(),
-                                size,
-                                val.iter()
-                                    .take(size.min(8))
-                                    .map(|x| format!("{}", x))
-                                    .collect::<Vec<String>>()
-                                    .join(", "),
-                                elide
-                            );
-                        } else if let Some(val) = value.as_double_array() {
-                            let size = val.size();
-                            let elide = if size < 8 { "" } else { "..." };
-                            println!(
-                                "  {}: {} = {}:[{}{}]",
-                                attr.name().text(),
-                                attr.type_name().as_token(),
-                                size,
-                                val.iter()
-                                    .take(size.min(8))
-                                    .map(|x| format!("{}", x))
-                                    .collect::<Vec<String>>()
-                                    .join(", "),
-                                elide
-                            );
-                        } else if let Some(val) = value.as_vec2_array() {
-                            let size = val.size();
-                            let elide = if size < 5 { "" } else { "..." };
-                            println!(
-                                "  {}: {} = {}:[{}{}]",
-                                attr.name().text(),
-                                attr.type_name().as_token(),
-                                size,
-                                val.iter()
-                                    .take(size.min(5))
-                                    .map(|x| format!("{}", x))
-                                    .collect::<Vec<String>>()
-                                    .join(", "),
-                                elide
-                            );
-                        } else if let Some(val) = value.as_vec3_array() {
-                            let size = val.size();
-                            let elide = if size < 5 { "" } else { "..." };
-                            println!(
-                                "  {}: {} = {}:[{}{}]",
-                                attr.name().text(),
-                                attr.type_name().as_token(),
-                                size,
-                                val.iter()
-                                    .take(size.min(5))
-                                    .map(|x| format!("{}", x))
-                                    .collect::<Vec<String>>()
-                                    .join(", "),
-                                elide
-                            );
-                        } else if let Some(val) = value.as_vec4_array() {
-                            let size = val.size();
-                            let elide = if size < 5 { "" } else { "..." };
-                            println!(
-                                "  {}: {} = {}:[{}{}]",
-                                attr.name().text(),
-                                attr.type_name().as_token(),
-                                size,
-                                val.iter()
-                                    .take(size.min(5))
-                                    .map(|x| format!("{}", x))
-                                    .collect::<Vec<String>>()
-                                    .join(", "),
-                                elide
-                            );
-                        } else {
-                            println!("  {}: {}", attr.name().text(), attr.type_name().as_token());
-                        }
-                    } else {
-                        println!("  {}: {}", attr.name().text(), attr.type_name().as_token());
-                    }
+                    print_attribute(&attr);
                 }
                 PropertyKind::Relationship(rel) => {
                     if let Some(targets) = rel.targets() {
                         let contents = targets
                             .iter()
-                            .map(|t| t.text())
-                            .collect::<Vec<&str>>()
+                            .map(|t| format!("<{}>", t.text()))
+                            .collect::<Vec<String>>()
                             .join(", ");
-                        println!("  {} = <{}>", rel.name().text(), contents);
+                        println!("  {} = [{}]", rel.name().text(), contents);
                     } else {
-                        println!("  {} = <>", rel.name().text());
+                        println!("  {} = []", rel.name().text());
                     };
                 }
             }
         }
+    }
+}
+
+fn print_attribute(attr: &Attribute) {
+    if let Some(value) = attr.get() {
+        if let Some(token) = value.as_token() {
+            println!(
+                "  {}: {} = {}",
+                attr.name().text(),
+                attr.type_name().as_token(),
+                token
+            );
+        } else if let Some(val) = value.as_asset_path() {
+            let path = if val.resolved_path().is_empty() {
+                val.asset_path()
+            } else {
+                val.resolved_path()
+            };
+
+            println!(
+                "  {}: {} = @{}@",
+                attr.name().text(),
+                attr.type_name().as_token(),
+                path
+            );
+        } else if let Some(val) = value.get::<i32>() {
+            println!(
+                "  {}: {} = {}",
+                attr.name().text(),
+                attr.type_name().as_token(),
+                val
+            );
+        } else if let Some(val) = value.get::<f32>() {
+            println!(
+                "  {}: {} = {}",
+                attr.name().text(),
+                attr.type_name().as_token(),
+                val
+            );
+        } else if let Some(val) = value.get::<f64>() {
+            println!(
+                "  {}: {} = {}",
+                attr.name().text(),
+                attr.type_name().as_token(),
+                val
+            );
+        } else if let Some(val) = value.get::<bool>() {
+            println!(
+                "  {}: {} = {}",
+                attr.name().text(),
+                attr.type_name().as_token(),
+                val
+            );
+        } else if let Some(val) = value.get::<Vec2>() {
+            println!(
+                "  {}: {} = {}",
+                attr.name().text(),
+                attr.type_name().as_token(),
+                val
+            );
+        } else if let Some(val) = value.get::<Vec3>() {
+            println!(
+                "  {}: {} = {}",
+                attr.name().text(),
+                attr.type_name().as_token(),
+                val
+            );
+        } else if let Some(val) = value.get::<Vec4>() {
+            println!(
+                "  {}: {} = {}",
+                attr.name().text(),
+                attr.type_name().as_token(),
+                val
+            );
+        } else if let Some(val) = value.as_token_array() {
+            println!(
+                "  {}: {} = [{}]",
+                attr.name().text(),
+                attr.type_name().as_token(),
+                val.iter()
+                    .map(|x| x.text())
+                    .collect::<Vec<&str>>()
+                    .join(", ")
+            );
+        } else if let Some(val) = value.as_int_array() {
+            let size = val.size();
+            let elide = if size < 8 { "" } else { "..." };
+            println!(
+                "  {}: {} = {}:[{}{}]",
+                attr.name().text(),
+                attr.type_name().as_token(),
+                size,
+                val.iter()
+                    .take(size.min(8))
+                    .map(|x| format!("{}", x))
+                    .collect::<Vec<String>>()
+                    .join(", "),
+                elide
+            );
+        } else if let Some(val) = value.as_float_array() {
+            let size = val.size();
+            let elide = if size < 8 { "" } else { "..." };
+            println!(
+                "  {}: {} = {}:[{}{}]",
+                attr.name().text(),
+                attr.type_name().as_token(),
+                size,
+                val.iter()
+                    .take(size.min(8))
+                    .map(|x| format!("{}", x))
+                    .collect::<Vec<String>>()
+                    .join(", "),
+                elide
+            );
+        } else if let Some(val) = value.as_double_array() {
+            let size = val.size();
+            let elide = if size < 8 { "" } else { "..." };
+            println!(
+                "  {}: {} = {}:[{}{}]",
+                attr.name().text(),
+                attr.type_name().as_token(),
+                size,
+                val.iter()
+                    .take(size.min(8))
+                    .map(|x| format!("{}", x))
+                    .collect::<Vec<String>>()
+                    .join(", "),
+                elide
+            );
+        } else if let Some(val) = value.as_vec2_array() {
+            let size = val.size();
+            let elide = if size < 5 { "" } else { "..." };
+            println!(
+                "  {}: {} = {}:[{}{}]",
+                attr.name().text(),
+                attr.type_name().as_token(),
+                size,
+                val.iter()
+                    .take(size.min(5))
+                    .map(|x| format!("{}", x))
+                    .collect::<Vec<String>>()
+                    .join(", "),
+                elide
+            );
+        } else if let Some(val) = value.as_vec3_array() {
+            let size = val.size();
+            let elide = if size < 5 { "" } else { "..." };
+            println!(
+                "  {}: {} = {}:[{}{}]",
+                attr.name().text(),
+                attr.type_name().as_token(),
+                size,
+                val.iter()
+                    .take(size.min(5))
+                    .map(|x| format!("{}", x))
+                    .collect::<Vec<String>>()
+                    .join(", "),
+                elide
+            );
+        } else if let Some(val) = value.as_vec4_array() {
+            let size = val.size();
+            let elide = if size < 5 { "" } else { "..." };
+            println!(
+                "  {}: {} = {}:[{}{}]",
+                attr.name().text(),
+                attr.type_name().as_token(),
+                size,
+                val.iter()
+                    .take(size.min(5))
+                    .map(|x| format!("{}", x))
+                    .collect::<Vec<String>>()
+                    .join(", "),
+                elide
+            );
+        } else {
+            println!("  {}: {}", attr.name().text(), attr.type_name().as_token());
+        }
+    } else {
+        println!("  {}: {}", attr.name().text(), attr.type_name().as_token());
     }
 }
