1c76251c9a1bc0f2cf2074ae5dd3ab051a473150
Anders Langlands <anderslanglands@gmail.com>
2024-01-12 19:23:32 +1300

    update bbl-build

--- build.rs
+++ build.rs
@@ -1,14 +1,10 @@
 use bbl_build::Config;
 
 pub fn main() {
-    let _dst = Config::new("openusd", "bbl-usd")
-        .generator("Ninja")
-        .profile("Release")
+    let _dst = Config::new("openusd", "../bbl-usd")
         .define("BBL_LANGUAGES", "rust")
-        .very_verbose(true)
-        .env("CMAKE_C_FLAGS", "")
-        .env("CMAKE_CXX_FLAGS", "")
         .build();
 
     println!("cargo:rerun-if-changed=../bbl-usd");
 }
+
