9dd0f3519ae54e260eefb98d88a19566fd4d386f
Anders Langlands <anderslanglands@gmail.com>
2024-01-12 19:23:43 +1300

    update bbl-usd

--- build.rs
+++ build.rs
@@ -1,10 +1,10 @@
 use bbl_build::Config;
 
 pub fn main() {
-    let _dst = Config::new("openusd", "../bbl-usd")
+    let _dst = Config::new("openusd", "bbl-usd")
         .define("BBL_LANGUAGES", "rust")
         .build();
 
-    println!("cargo:rerun-if-changed=../bbl-usd");
+    println!("cargo:rerun-if-changed=bbl-usd");
 }
 
